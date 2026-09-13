59fcf11c33ac8dd2daded14f7b97a95892792058
Himanshuagarwal21 <agarwalhimanshu81453@gmail.com>
2026-09-12 22:56:01 -0400

    update api url for online and local host

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,5 +1,8 @@
-const API = "https://codsoft-taskno1.onrender.com";
-
+const API =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
+        ? "http://localhost:8080"
+        : "https://codsoft-taskno1.onrender.com";
 let attempts = 0;
 
 function startGame() {
